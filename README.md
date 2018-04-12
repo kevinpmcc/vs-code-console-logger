ade7bdbcf5061e3fe3faed1c0397861a576e0641
Kevin McCarthy <kevinpatrickmccarthy@gmail.com>
2018-04-12 19:19:26 +0100

    initial commit
    - works by highlighting text, Shift+Cmd+P and choose Console Logger

---
+++ README.md
@@ -0,0 +1,26 @@
+# consolelogger README
+
+This is the README for the vs code extension "consoleLogger". 
+
+This will all be filled out in due course
+
+## Features
+
+
+
+## Requirements
+
+
+
+## Extension Settings
+
+
+## Known Issues
+
+
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 1.0.0
---
+++ extension.js
@@ -0,0 +1,39 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('extension.consoleLogger', function () {
+        // The console.log('', )code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        var editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
+
+        var selection = editor.selection;
+        var text = editor.document.getText(selection);
+
+        var newText = "console.log('" + text + "', " + text + ")" 
+
+        // Display a message box to the user
+           editor.edit(builder => {
+            builder.replace(selection, newText)
+        })
+    });
+
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {
+}
+exports.deactivate = deactivate;
